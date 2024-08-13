f93954901c6cf6ea16cd5c1b780f0cac3fd065cd
Chuck Norris <me@example.com>
2024-08-13 09:12:51 +0200

    implemented mode7

---
+++ Mode7.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "olcPixelGameEngine.h"
+
+class Mode7 {
+public:
+	olc::Sprite* floor;
+	olc::Sprite* roof;
+	float fFar;
+	float fNear;
+	float halfFov;
+
+	void draw(olc::PixelGameEngine& canvas, const olc::vf2d& pos, float angle) {
+		olc::vf2d near1 = { pos.x + cosf(angle - halfFov) * fNear, pos.y + sinf(angle - halfFov) * fNear };
+		olc::vf2d near2 = { pos.x + cosf(angle + halfFov) * fNear, pos.y + sinf(angle + halfFov) * fNear };
+		olc::vf2d far1 = { pos.x + cosf(angle - halfFov) * fFar, pos.y + sinf(angle - halfFov) * fFar };
+		olc::vf2d far2 = { pos.x + cosf(angle + halfFov) * fFar, pos.y + sinf(angle + halfFov) * fFar };
+
+		float hScrn = canvas.ScreenHeight() / 2;
+		for (int y = 0; y < hScrn; y++) {
+			float sampleDepth = y / hScrn;
+			olc::vf2d xpos1 = (far1 - near1) / sampleDepth + near1;
+			olc::vf2d xpos2 = (far2 - near2) / sampleDepth + near2;
+			for (int x = 0; x < canvas.ScreenWidth(); x++) {
+				float sampleWidth = x / (float)canvas.ScreenHeight();
+				olc::vf2d samplePos = (xpos2 - xpos1) * sampleWidth + xpos1;
+				
+				if (floor) {
+					canvas.Draw(x, y, floor->GetPixel(samplePos));
+				}
+
+				if (roof) {
+					canvas.Draw(x, y, roof->GetPixel(samplePos));
+				}
+			}
+		}
+	}
+
+	void setFOV(float fov) {
+		halfFov = fov / 2;
+	}
+
+	Mode7(olc::Sprite* floor, olc::Sprite* roof, float fFar, float fNear, float halfFov)
+		: floor(floor), roof(roof), fFar(fFar), fNear(fNear), halfFov(halfFov)
+	{
+	}
+	Mode7() {}
+};
+
+//void mode7(olc::PixelGameEngine& canvas, const olc::Sprite* floor, const olc::Sprite* roof) {
+//	
+//}
--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
-
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#include "Mode7.h"
+#include <memory>
+
+struct Player {
+	olc::vf2d pos;
+	float angle;
+};
 
 // Override base class with your custom functionality
 class Window : public olc::PixelGameEngine
 {
+	Mode7 mode7;
+	Player player;
+
 public:
-	Window()
+	Window() : mode7(nullptr, nullptr, 1, 0.1, 3.1415/4)
 	{
 		// Name your application
 		sAppName = "Window";
@@ -16,15 +25,16 @@ public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
+		mode7.floor = new olc::Sprite("track.jpg");
+		mode7.roof = new olc::Sprite("sky.jpg");
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		// Called once per frame, draws random coloured pixels
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+		//std::cout << mode7.floor->width<<' '<<mode7.roof->width<<'\n';
+		mode7.draw(*this, player.pos, player.angle);
 		return true;
 	}
 };
